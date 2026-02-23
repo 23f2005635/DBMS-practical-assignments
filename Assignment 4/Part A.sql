f17eedb8c8290c09238cb09c2d08847ed64a2ef4
Prince Kumar <prince857900@gmail.com>
2026-02-23 15:15:13 +0530

    update

---
+++ Assignment 4/Part A.sql
@@ -0,0 +1,9 @@
+select Student_Id as registration_num,
+    Name as Student_name,
+     Gender as Sex ,
+    Date_of_birth as Dob,
+    Contact_Number as Num,
+    Department_Id as Dep_Id
+    from Student;
+
+

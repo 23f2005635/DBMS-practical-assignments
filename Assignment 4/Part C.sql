2af59c8ec86508b170394692b1f56914a0515577
Prince Kumar <prince857900@gmail.com>
2026-02-23 16:30:55 +0530

    update assignment 4

--- Assignment 4/Part C.sql
+++ Assignment 4/Part C.sql
@@ -14,4 +14,16 @@ select *
   3  order by Designation Asc;
 
 
+select *
+  2  from Course
+  3  order by Credits Desc;
+
+  select *
+  2  from Student
+  3  Fetch first 3 Rows only;
+
+   select *
+  2  from Course
+  3  Fetch first 5 Rows only;
+
   
---
+++ Assignment 4/Part D.sql
@@ -0,0 +1,4 @@
+ select Name, TRUNC(SYSDATE - Date_of_Birth) as age_in_days
+  2  from Student;
+
+  
